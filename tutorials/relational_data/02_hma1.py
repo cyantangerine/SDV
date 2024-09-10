118bef86e74af72a72c06f04df8e515da55a081b
wbx <1487819688@qq.com>
2024-09-10 21:17:45 +0800

    1

--- tutorials/relational_data/02_hma1.py
+++ tutorials/relational_data/02_hma1.py
@@ -1,8 +1,45 @@
+from sdv.relational import HMA1
+import sys
+import warnings
+warnings.filterwarnings('ignore')
+#from tutorials.newhma import NewModel as HMA1
+#from tutorials.redirector import WriteableRedirector
 import warnings
 from sdv.relational import HMA1
-from tutorials.relational_data.test_20_tables import save_tables
+from test_20_tables import save_tables
+import time
+from sdv import Metadata
+import os
 
-warnings.filterwarnings('ignore')
-loaded = HMA1.load('my_model_10k.pkl')
-new_data = loaded.sample()
-save_tables(new_data, "output_10k.xlsx")
+print(os.getcwd())
+from test_20_tables import fetch_data_from_sqlite
+times = []
+last_time = time.time()
+def addt():
+    t = time.time()
+    times.append(t-last_time)
+    last_time = t
+
+metadata, tables = fetch_data_from_sqlite()
+metadata = Metadata(metadata)
+for name, table in tables.items():
+    print(name, table.shape)
+from test_20_tables import save_tables
+save_tables(tables, "input.xlsx")
+
+addt()
+
+#with WriteableRedirector():
+model = HMA1(metadata)    
+model.fit(tables)
+
+addt()
+
+model.save('my_model_10k.pkl')
+
+addt()
+
+new_data = model.sample()
+save_tables(new_data)
+
+addt()
